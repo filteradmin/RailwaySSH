0d6fe24c29b29fbbdcb6b0c798b51b1d095348c8
filteradmin <87537438+filteradmin@users.noreply.github.com>
2023-11-11 04:36:32 +0600

    .

---
+++ info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat << "EOF"
+
+   _____         ______    ____  _   _ ______ 
+  / ____|  /\   |  ____|  / __ \| \ | |  ____|
+ | (___   /  \  | |__    | |  | |  \| | |__   
+  \___ \ / /\ \ |  __|   | |  | | . ` |  __|  
+  ____) / ____ \| |      | |__| | |\  | |____ 
+ |_____/_/    \_\_|       \____/|_| \_|______|
+                                              
+This server is hosted by Safone. If you have any questions or need help,
+please don't hesitate to contact us at support@safone.dev. Enjoy !!
+
+EOF
